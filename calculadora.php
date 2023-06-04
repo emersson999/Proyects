8825569f3cf09e50d280587add55cbabc16d838a
emersson999 <nossreme1997@hotmail.com>
2023-06-04 15:30:09 -0500

    Back

--- calculadora.php
+++ calculadora.php
@@ -5,10 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/df0f283818.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="css/calculadora.css">
-    <title>Document</title>
+    <title>Calculadora</title>
 </head>
 <body>
+    <a class="home" href="index.html#section-respuestas">
+        <i class="fa-solid fa-house fa-2xl"></i>
+    </a>
    <div class="main-frame-cal">
    <form action="" method="POST">
         <div class="form-group">
@@ -26,6 +32,7 @@
             <option value="2">Resta</option>
             <option value="3">Multiplicación</option>
             <option value="4">División</option>
+            <a><i class="bi bi-house"></i></a>
             </select>
         </div>
         <button type="submit" name="operar" class="btn btn-outline-info">Operar</button>
@@ -57,5 +64,6 @@ if (isset($_POST['operar'])) {
 }
 ?>
    </div>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
